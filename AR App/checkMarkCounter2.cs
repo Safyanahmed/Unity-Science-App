cc67b50a549b32857a59120cce0e2c04bbd86f9b
Safyan Ahmed <safyanahmed123@gmail.com>
2021-03-27 23:03:04 +0000

    Add files via upload

---
+++ AR App/checkMarkCounter2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class checkMarkCounter2 : MonoBehaviour
+{
+
+    public GameObject[] checkMarks;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int checkMarkCounter = 0;
+      foreach (var mark in checkMarks)
+        {
+            if(mark.GetComponent<Renderer>().enabled)
+               {
+                checkMarkCounter++;
+            }
+        }
+
+       
+        if (checkMarkCounter == 5)
+        {
+            Debug.Log("Done!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        Debug.Log(checkMarkCounter);
+    }
+}
---
+++ Desktop App/checkMarkCounter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class checkMarkCounter : MonoBehaviour
+{
+
+    public GameObject[] checkMarks;
+    public GameObject win;
+    public GameObject continueButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        continueButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int checkMarkCounter = 0;
+      foreach (var mark in checkMarks)
+        {
+            if(mark.GetComponent<SpriteRenderer>().enabled)
+               {
+                checkMarkCounter++;
+            }
+        }
+
+      if(checkMarkCounter == 3)
+        {
+            Debug.Log("Done!");
+           
+            win.GetComponent<Renderer>().enabled = true;
+            continueButton.SetActive(true);
+        }
+        Debug.Log(checkMarkCounter);
+    }
+}
